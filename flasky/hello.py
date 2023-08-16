2a0b0c2f58230a1d71e1577b60459dc3cedddc56
William <68653853+will-i-amv@users.noreply.github.com>
2023-08-16 12:43:34 -0400

    Add first 2 HTML templates and modify the view functions to render them

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello, {name}!</h1>'
+    return render_template('user.html', name=name)
