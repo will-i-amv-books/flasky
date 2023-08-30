11ce885387f3b6946abdf6e609a5563497a2733a
William <68653853+will-i-amv@users.noreply.github.com>
2023-08-30 11:49:26 -0400

    Changes: * Use 'redirect()' if the form is filled, to implement the Post/Redirect/Get pattern. * Use the session dict to store the user name, so it's not lost after the request ends.

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, session, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -21,12 +21,11 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
