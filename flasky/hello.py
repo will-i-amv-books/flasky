303c6107b2e443d4dcbf6201bc249583c324daa5
William <68653853+will-i-amv@users.noreply.github.com>
2023-08-30 14:44:04 -0400

    Add lazy='dynamic' to the 'users' attribute, this allows us to build complex queries when calling the attribute, instead of a querying to obtain all the rows as default behavior.

--- flasky/hello.py
+++ flasky/hello.py
@@ -55,13 +55,25 @@ class NameForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+    if not form.validate_on_submit():
+        return render_template(
+            'index.html',
+            form=form, 
+            name=session.get('name'),
+            known=session.get('known', False)
+        )
+    else:
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is not None:
+            session['known'] = True
+        else:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
