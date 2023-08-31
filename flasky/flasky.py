e5c75486f4594ea95bbc713c3f3e880941e130ea
William <68653853+will-i-amv@users.noreply.github.com>
2023-08-31 12:16:46 -0400

    Changes: * Add first 2 basic test cases * Add a command option 'test' to execute the new tests

--- flasky/flasky.py
+++ flasky/flasky.py
@@ -11,3 +11,11 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Role': Role}
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
