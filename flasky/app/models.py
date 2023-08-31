997bd96451ab7e0e297984e0d9e8357e1e532ba7
William <68653853+will-i-amv@users.noreply.github.com>
2023-08-31 15:24:19 -0400

    Add email field to 'User' model

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -21,6 +21,7 @@ class User(db.Model):
         unique=True,
         index=True
     )
+    email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
