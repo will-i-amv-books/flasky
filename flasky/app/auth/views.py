e2b2dd8982bb9e4e807f1d06f251da8034173dc4
William <68653853+will-i-amv@users.noreply.github.com>
2023-09-01 10:21:30 -0400

    Refactor login, check for special cases in the 'if' conditions, leave the default case in 'else'.

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -9,16 +9,21 @@ from .forms import LoginForm
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    if not form.validate_on_submit():
+        return render_template('auth/login.html', form=form)
+    else:
         user = User.query.filter_by(email=form.email.data).first()
-        if (
+        if not (
             (user is not None) and 
             (user.verify_password(form.password.data))
         ):
+            flash('Invalid username or password.')
+            return redirect(url_for('auth.login'))
+        else:
             login_user(user, form.remember_me.data)
-            next = request.args.get('next')
-            if next is None or not next.startswith('/'):
-                next = url_for('main.index')
+            next = (
+                request.args.get('next')
+                if (next is None) or (not next.startswith('/')) else
+                url_for('main.index')
+            )
             return redirect(next)
-        flash('Invalid username or password.')
-    return render_template('auth/login.html', form=form)
